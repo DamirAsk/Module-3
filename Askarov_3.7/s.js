a21677a26edbefd6a7318a5d937f01e48b969bc4
Damir <damirpuga4ev@gmail.com>
2024-01-17 00:45:00 +0300

    a

---
+++ Askarov_3.7/s.js
@@ -0,0 +1,5 @@
+function Geron(a, b, c) {
+    let p = (a+b+c) / 2;
+    return (p * (p - a) * (p - b) * (p - c))**0.5
+}
+document.write(Geron(Number(prompt()), Number(prompt()), Number(prompt())))
